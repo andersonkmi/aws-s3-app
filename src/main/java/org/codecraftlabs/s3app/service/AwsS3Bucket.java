c2bf3052b7f88d93b36727d4d2c7c85d70008dff
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2019-12-30 00:17:26 +0100

    more files

---
+++ src/main/java/org/codecraftlabs/s3app/service/AwsS3Bucket.java
@@ -0,0 +1,11 @@
+package org.codecraftlabs.s3app.service;
+
+public class AwsS3Bucket {
+    public void create(String bucketName) {
+
+    }
+
+    public void remove(String bucketName) {
+
+    }
+}

e1aa7574b1e5f1d8bf4193bd103710d891f2fa10
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-01-05 11:06:40 +0100

    removed unused method

--- src/main/java/org/codecraftlabs/s3app/service/S3BucketListService.java
+++ src/main/java/org/codecraftlabs/s3app/service/S3BucketListService.java
@@ -14,15 +14,10 @@ import javax.annotation.Nonnull;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import static org.codecraftlabs.s3app.data.AWSRegion.US_EAST_1;
 import static org.codecraftlabs.s3app.util.AWSRegionMapper.awsRegion;
 
 public class S3BucketListService {
     private static final Logger logger = LogManager.getLogger(S3BucketListService.class);
-
-    public Set<S3Bucket> buckets() throws AWSException {
-        return buckets(US_EAST_1);
-    }
 
     public Set<S3Bucket> buckets(@Nonnull final AWSRegion region) throws AWSException {
         try {

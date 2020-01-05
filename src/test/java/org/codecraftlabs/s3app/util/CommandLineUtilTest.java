9f65e932bde16f964f87cff2843284e5007a2cfe
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-01-05 12:14:24 +0100

    more tests

--- src/test/java/org/codecraftlabs/s3app/util/CommandLineUtilTest.java
+++ src/test/java/org/codecraftlabs/s3app/util/CommandLineUtilTest.java
@@ -1,5 +1,6 @@
 package org.codecraftlabs.s3app.util;
 
+import org.apache.commons.cli.CommandLineParser;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,6 +14,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.hasKey;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayName("CommandLineUtil class unit tests")
 public class CommandLineUtilTest {
@@ -35,5 +37,31 @@ public class CommandLineUtilTest {
         assertThat(options, hasEntry(AWS_REGION_LONG_OPT, "us-east-1"));
         assertThat(options, hasKey(S3_BUCKET_NAME_LONG_OPT));
         assertThat(options, hasEntry(S3_BUCKET_NAME_LONG_OPT, "test"));
+    }
+
+    @Test
+    @DisplayName("When the args are all ok")
+    void parseArgsMinimumRequiredOk() {
+        String[] args = {"-s", "listBucket", "-r", "us-east-1"};
+        assertDoesNotThrow(() -> service.parse(args));
+        Map<String, String> options = service.options();
+        assertThat(options, hasKey(S3_SERVICE_LONG_OPT));
+        assertThat(options, hasEntry(S3_SERVICE_LONG_OPT, "listBucket"));
+        assertThat(options, hasKey(AWS_REGION_LONG_OPT));
+        assertThat(options, hasEntry(AWS_REGION_LONG_OPT, "us-east-1"));
+    }
+
+    @Test
+    @DisplayName("When -s option is missing")
+    void missingServiceOption() {
+        String[] args = {"-r", "us-east-1", "-b", "test"};
+        assertThrows(CommandLineException.class, () -> service.parse(args));
+    }
+
+    @Test
+    @DisplayName("When -r option is missing")
+    void missingRegionOption() {
+        String[] args = {"-s", "listBucket", "-b", "test"};
+        assertThrows(CommandLineException.class, () -> service.parse(args));
     }
 }

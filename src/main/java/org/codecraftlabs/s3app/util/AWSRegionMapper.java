c07cae66db1514161774a3c7e6697b821e1f550c
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-01-12 11:56:55 +0100

    minor refactoring

--- src/main/java/org/codecraftlabs/s3app/util/AWSRegionMapper.java
+++ src/main/java/org/codecraftlabs/s3app/util/AWSRegionMapper.java
@@ -7,15 +7,18 @@ import software.amazon.awssdk.regions.Region;
 
 import javax.annotation.Nonnull;
 
+import static software.amazon.awssdk.regions.Region.US_EAST_1;
+import static software.amazon.awssdk.regions.Region.of;
+
 public class AWSRegionMapper {
     private static final Logger logger = LogManager.getLogger(AWSRegionMapper.class);
 
     public static Region awsRegion(@Nonnull AWSRegion from) {
         logger.debug(String.format("Mapping region '%s - %s'", from.code(), from.description()));
-        var to = Region.of(from.code());
+        var to = of(from.code());
         if (to == null) {
             logger.info("Could not map to an existing AWS region. Defaulting to US_EAST_1");
-            return Region.US_EAST_1;
+            return US_EAST_1;
         }
         return to;
     }
